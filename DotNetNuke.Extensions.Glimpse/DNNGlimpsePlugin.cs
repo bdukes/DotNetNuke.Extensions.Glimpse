06782bd305b8e4153cc850ab7b7b51f9fda0ef7d
Brian Dukes <bdukes@engagesoftware.com>
2013-03-26 12:08:52 -0500

    Fix error serializing context items
    Also change format of output to be grouped

--- DotNetNuke.Extensions.Glimpse/DNNGlimpsePlugin.cs
+++ DotNetNuke.Extensions.Glimpse/DNNGlimpsePlugin.cs
@@ -28,44 +28,45 @@ namespace DotNetNuke.Extensions.Glimpse
             try
             {
                 var portalSettings = PortalSettings.Current;
-                var tabCreatedByUser = UserController.GetUserById(-1, portalSettings.ActiveTab.CreatedByUserID);
-                var tabModifiedByUser = UserController.GetUserById(-1, portalSettings.ActiveTab.LastModifiedByUserID);
                 var portalAliases = from PortalAliasInfo pa in new PortalAliasController().GetPortalAliasArrayByPortalID(portalSettings.PortalId)
                                     select pa.HTTPAlias;
-
                 var httpContext = context.GetRequestContext<HttpContextBase>();
-                var contextItems = (from string key in httpContext.Items.Keys 
-                                    select new[] { key, httpContext.Items[key] }).ToList();
-
-                return new List<object[]>
-                           {
-                               new object[] { "Property", "Value" },
-                               new object[] { "Portal ID", portalSettings.PortalId },
-                               new object[] { "Portal Name", portalSettings.PortalName },
-                               new object[] { "Portal Aliases", portalAliases.ToArray() },
-                               new object[] { "Portal SSL Enabled", portalSettings.SSLEnabled },
-                               new object[] { "Portal SSL Enforced", portalSettings.SSLEnforced },
-                               new object[] { "User ID", portalSettings.UserId },
-                               new object[] { "User Name", portalSettings.UserInfo.Username },
-                               new object[] { "User Roles", portalSettings.UserInfo.Roles },
-                               new object[] { "Tab ID", portalSettings.ActiveTab.TabID },
-                               new object[] { "Tab Name", portalSettings.ActiveTab.TabName },
-                               new object[] { "Tab Title", portalSettings.ActiveTab.Title },
-                               new object[] { "Tab Path", portalSettings.ActiveTab.TabPath },
-                               new object[] { "Tab SSL Enabled", portalSettings.ActiveTab.IsSecure },
-                               new object[] { "Tab Created By", (tabCreatedByUser == null) ? null : tabCreatedByUser.Username },
-                               new object[] { "Tab Created Date", portalSettings.ActiveTab.CreatedOnDate },
-                               new object[] { "Tab Modified By", (tabModifiedByUser == null) ? null : tabModifiedByUser.Username },
-                               new object[] { "Tab Modified Date", portalSettings.ActiveTab.LastModifiedOnDate },
-                               new object[] { "Tab Skin Path", portalSettings.ActiveTab.SkinPath },
-                               new object[] { "Tab Skin Source", portalSettings.ActiveTab.SkinSrc },
-                               new object[] { "Context Items", contextItems },
-                           };
+                var contextItems = from string key in httpContext.Items.Keys
+                                   let value = httpContext.Items[key]
+                                   where !key.StartsWith("__Glimpse")
+                                   orderby key
+                                   select new { key, value = value == null ? null : value is IDictionary<string, string> ? value : value.ToString() };
+                return new {
+                    Portal = new {
+                        ID = portalSettings.PortalId,
+                        Name = portalSettings.PortalName,
+                        Aliases = portalAliases.ToArray(),
+                        SSL = new { 
+                            Enabled = portalSettings.SSLEnabled,
+                            Enforced = portalSettings.SSLEnforced,
+                        },
+                    },
+                    User = new {
+                        ID = portalSettings.UserId,
+                        Username = portalSettings.UserInfo.Username,
+                        Roles = portalSettings.UserInfo.Roles,
+                    },
+                    Tab = new {
+                        ID = portalSettings.ActiveTab.TabID,
+                        Name = portalSettings.ActiveTab.TabName,
+                        Title = portalSettings.ActiveTab.Title,
+                        Path = portalSettings.ActiveTab.TabPath,
+                        Secure = portalSettings.ActiveTab.IsSecure,
+                        SkinPath = portalSettings.ActiveTab.SkinPath,
+                        SkinSource = portalSettings.ActiveTab.SkinSrc,
+                    },
+                    Context = contextItems.ToArray(),
+                };
             }
             catch (Exception ex)
             {
                 Exceptions.LogException(ex);
-                return ex.Message + ex.StackTrace;
+                return "There was an error rendering this tab";
             }
         }
     }
