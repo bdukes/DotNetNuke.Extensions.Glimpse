0115c026eb98a2c91591cf0256f55de523165b33
Brian Dukes <bdukes@engagesoftware.com>
2013-04-22 23:41:48 -0500

    Add copyright

--- DotNetNuke.Extensions.Glimpse/Properties/AssemblyInfo.cs
+++ DotNetNuke.Extensions.Glimpse/Properties/AssemblyInfo.cs
@@ -1,5 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// <copyright file="AssemblyInfo.cs" company="DotNetNuke Corp.">
+// DotNetNuke.Extensions.Glimpse
+// Copyright (c) 2013
+// by DotNetNuke Corp.
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +18,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DotNetNuke.Extensions.Glimpse")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("DotNetNuke Corp.")]
 [assembly: AssemblyProduct("DotNetNuke.Extensions.Glimpse")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © DotNetNuke Corp. 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
